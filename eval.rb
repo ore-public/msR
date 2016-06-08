e3e03d5ad0b193ce4ca0174685c7cf294cf41567
Masaya Konishi <konishi@office-ore.net>
2016-06-08 13:03:32 +0900

    配列の式表現を評価出来るように

---
+++ eval.rb
@@ -0,0 +1,56 @@
+def list?(exp)
+  exp.is_a?(Array)
+end
+
+$primitive_fun_env = {
+    :+ => [:prime, lambda{|x, y| x + y}],
+    :- => [:prime, lambda{|x, y| x - y}],
+    :* => [:prime, lambda{|x, y| x * y}],
+}
+
+def lookup_primitive_fun(exp)
+  $primitive_fun_env[exp]
+end
+
+def car(list)
+  list[0]
+end
+
+def cdr(list)
+  list[1..-1]
+end
+
+def eval_list(exp)
+  exp.map{|e| _eval(e)}
+end
+
+def immediate_val?(exp)
+  num?(exp)
+end
+
+def num?(exp)
+  exp.is_a?(Numeric)
+end
+
+def apply(fun, args)
+  apply_primitive_fun(fun, args)
+end
+
+def apply_primitive_fun(fun, args)
+  fun_val = fun[1]
+  fun_val.call(*args)
+end
+
+def _eval(exp)
+  if not list?(exp)
+    if immediate_val?(exp)
+      exp
+    else
+      lookup_primitive_fun(exp)
+    end
+  else
+    fun = _eval(car(exp))
+    args = eval_list(cdr(exp))
+    apply(fun, args)
+  end
+end
